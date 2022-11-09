0a2afb50e84413b2e86e624ce085e7d20d614eae
Emile Fugulin <code@efugulin.com>
2022-11-09 17:25:08 -0500

    Add placeholder code for windows

--- src/windows/mod.rs
+++ src/windows/mod.rs
@@ -1,4 +1,8 @@
+use std::mem::MaybeUninit;
+
+use winapi::shared::minwindef::LPDWORD;
 use winapi::um::handleapi::CloseHandle;
+use winapi::um::minwinbase::STILL_ACTIVE;
 use winapi::um::processthreadsapi::{GetExitCodeProcess, OpenProcess};
 
 use self::handle::Handle;
@@ -7,5 +11,21 @@ use crate::{Pid, State};
 mod handle;
 
 pub fn state(pid: Pid) -> State {
-    todo!()
+    let handle = match Handle::open(pid) {
+        Some(handle) => handle,
+        None => State::Unknown,
+    };
+
+    let mut status = MaybeUninit::uninit();
+    unsafe {
+        if !GetExitCodeProcess(*handle, status.as_mut_ptr()) {
+            return Status::Unknown;
+        }
+
+        if status.assume_init() == STILL_ACTIVE {
+            Status::Alive
+        } else {
+            Status::Dead
+        }
+    }
 }
