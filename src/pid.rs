32f6bc79680bbde4b23cd9c9c86109c82d29dc7f
Emile Fugulin <emile@caido.io>
2022-11-10 12:39:38 -0500

    Add try_from i32 for Pid

--- src/pid.rs
+++ src/pid.rs
@@ -1,11 +1,21 @@
 use std::fmt;
+use std::num::TryFromIntError;
 
+/// Used as a wrapper for the process ID
 #[derive(Clone, Copy, PartialEq, Eq)]
 pub struct Pid(pub(crate) u32);
 
 impl From<u32> for Pid {
     fn from(value: u32) -> Self {
         Self(value)
+    }
+}
+
+impl TryFrom<i32> for Pid {
+    type Error = TryFromIntError;
+
+    fn try_from(value: i32) -> Result<Self, Self::Error> {
+        Ok(Self(value.try_into()?))
     }
 }
 
